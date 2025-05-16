624ab7f7747518e5d088b3f7dd8034e106360df0
dipudan <dipudan@gmail.com>
2025-05-16 10:03:59 +0530

    Made run headless and incognito

--- conftest.py
+++ conftest.py
@@ -6,8 +6,7 @@ from selenium import webdriver
 def setUp_tearDown():
     option = webdriver.ChromeOptions()
     option.add_argument("--start-maximized")
-    option.add_argument("--disable-save-password-bubble")
-    option.add_argument("--password-store=basic")
+    option.add_argument("--incognito")
     option.add_argument("--headless")
     driver = webdriver.Chrome(options=option)
     yield driver
